ba0abea2a592c14cfd0ba76f3da96e18b21eb15c
robespierre87 <robespierre87@gmail.com>
2013-03-26 19:34:27 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conqclub::Application.config.secret_token = 'b7f75ebd07fa9717d602b8b938dbdcc80a9a937c1fb10344a72af4c08b01a406426128e82fa4d2cae468a36291039271fdb695abcc67bae682cf23c251ea6e32'
